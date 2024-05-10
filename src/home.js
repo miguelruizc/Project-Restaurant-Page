3dc06e7637161f702ee6fa996c83c84e18425248
miguelruizc <buumchakalaka@gmail.com>
2024-05-10 05:01:06 +0200

    Add generateHome function, that generates home-page

---
+++ src/home.js
@@ -0,0 +1,19 @@
+export function generateHome () {
+    // Get container to append elements
+    let container = document.getElementById("content");
+
+    // Create HTML elements for home page & append
+    // Image
+    let img = document.createElement("img");
+    img.src = "https://picsum.photos/500/400"
+    container.appendChild(img);
+    // headline
+    let h = document.createElement("h1");
+    h.textContent = "Super Amazing Restaurant";
+    container.appendChild(h);
+    // paragraph
+    let p = document.createElement("p");
+    p.textContent = "Very cool restaurant because adipisicing elit. Inventore similique sapiente culpa magni provident molestiae reiciendis minima labore eum libero, eveniet obcaecati officiis repellat perferendis quasi aspernatur blanditiis saepe quia!";
+    container.appendChild(p);
+}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("Hello webpack");
+import {generateHome} from './home.js';
+
+generateHome();
