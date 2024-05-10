8f62431aeaca15e4c8288ee89d5635731bb39bed
miguelruizc <buumchakalaka@gmail.com>
2024-05-10 15:44:01 +0200

    Add tab-switching logic to navigate the site

--- src/index.js
+++ src/index.js
@@ -2,7 +2,28 @@ import { generateHome } from './home.js';
 import { generateMenu } from './menu.js';
 import { generateContact } from './contact.js';
 
-//generateHome();
-//generateMenu();
-generateContact();
+function clearDom() {
+    document.getElementById("content").innerHTML = "";
+}
+
+let homeBtn = document.getElementById("home");
+homeBtn.addEventListener("click", function(){
+    clearDom();
+    generateHome();
+
+});
+
+let menuBtn = document.getElementById("menu");
+menuBtn.addEventListener("click", function(){
+    clearDom();
+    generateMenu();
+});
+
+let contactBtn = document.getElementById("contact");
+contactBtn.addEventListener("click", () => {
+    clearDom();
+    generateContact();
+});
+
+generateHome();
 
